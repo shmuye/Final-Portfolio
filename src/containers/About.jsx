c413c38a3c0c6c1cfd533501f2437126c6cece0b
Shumye Ayalneh <shmuye27@gmail.com>
2026-01-23 22:27:40 +0300

    update: resume link

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -59,6 +59,7 @@ const About = () => {
                         {/* Buttons */}
                         <div className="mt-4 flex items-center gap-4">
                             <a
+                                target="/"
                                 href="https://drive.google.com/file/d/12Gpxa328Z-781uJqyAUvPaCdYMaoFVo5/view?usp=sharing"
                                 className="bg-brand text-white p-3 text-sm whitespace-nowrap hover:bg-brand-dark transition"
                             >
